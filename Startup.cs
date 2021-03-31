53680a4ca6134b4fdfa61b27fc1ae979abae1dc0
Federico Pelliccia <pellicciafederico@gmail.com>
2021-03-31 18:25:43 +0200

    Aggiunte librerie bootstrap e fontawesom locali

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Mvc;
+using Westwind.AspNetCore.LiveReload;
 
 namespace ELearningFake
 {
@@ -16,6 +17,7 @@ namespace ELearningFake
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLiveReload();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -27,6 +29,7 @@ namespace ELearningFake
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseLiveReload();
             app.UseStaticFiles();
 
 
