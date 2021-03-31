ac4e595266addfcf973b9f767aaf65d88c71c7d9
Federico Pelliccia <pellicciafederico@gmail.com>
2021-03-31 13:41:23 +0200

    Aggiornata l'applicazione ad Asp.Net core 2.2

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ELearningFake
 {
@@ -15,7 +16,7 @@ namespace ELearningFake
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
